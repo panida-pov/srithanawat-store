61166cc890075c6dd9b5e0e2f045ecdd7c4444be
panida-pov <panida.pov@gmail.com>
2024-04-26 20:27:37 +0900

    correct syntax

--- client/src/pages/ContactPage/ContactPage.jsx
+++ client/src/pages/ContactPage/ContactPage.jsx
@@ -10,9 +10,9 @@ function ContactPage() {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3872.4747020141463!2d100.50403817563279!3d13.930317193134321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e28442ace7a45d%3A0x8bf5cadb4214ee6f!2z4Lio4Lij4Li14LiY4LiZ4Liy4Lin4Lix4LiS4LiZ4LmM!5e0!3m2!1sen!2sjp!4v1714030590838!5m2!1sen!2sjp"
           width="400"
           height="300"
-          allowfullscreen
+          allowFullScreen
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
     </div>
