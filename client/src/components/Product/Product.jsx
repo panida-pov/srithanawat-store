d9b4756bc406d92ba51e11288a2d625164190327
panida-pov <panida.pov@gmail.com>
2024-05-02 19:54:01 +0900

    create product details component

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,30 +1,38 @@
+import { useState } from "react";
+import ProductDetails from "../ProductDetails/ProductDetails";
 import styles from "./Product.module.css";
 
-function Product() {
-  const sample = {
-    id: 1,
-    name: "ข้องอ 90 องศา 1 นิ้ว",
-    category: "ข้อต่อPVC",
-    color: "สีฟ้า",
-    brand: "ท่อน้ำไทย",
-    price: 12,
-    details: "หนา 1 นิ้ว",
-    image_url: "./assets/images/product_1.jpg",
+function Product({ product }) {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const closeDetails = () => {
+    setShowDetails(false);
   };
 
   return (
-    <div className={styles.product}>
-      <div className={styles.overlay}>
-        <div className={styles.image}>
-          <img src={sample.image_url} alt={sample.name} />
+    <>
+      <div className={styles.product} onClick={() => setShowDetails(true)}>
+        <div className={styles.overlay}>
+          <div className={styles.image}>
+            <img
+              src={product.image_url}
+              alt={product.name}
+              onError={({ target }) =>
+                (target.src = "./assets/images/no_img.jpg")
+              }
+            />
+          </div>
+          <div className={styles.view}>
+            <div className={styles.text}>Click to view</div>
+          </div>
         </div>
-        <div className={styles.view}>
-          <div className={styles.text}>Click to view</div>
-        </div>
+        <p className={styles.productName}>{product.name}</p>
+        <p className={styles.productPrice}>{`฿${product.price || " -"}`}</p>
       </div>
-      <p className={styles.productName}>{sample.name}</p>
-      <p className={styles.productPrice}>{`฿${sample.price.toFixed(2)}`}</p>
-    </div>
+      {showDetails && (
+        <ProductDetails product={product} closeDetails={closeDetails} />
+      )}
+    </>
   );
 }
 
---
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,41 @@
+import styles from "./ProductDetails.module.css";
+import DisabledByDefaultIcon from "@mui/icons-material/DisabledByDefault";
+
+function ProductDetails(props) {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.box}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={props.closeDetails}
+        >
+          <DisabledByDefaultIcon style={{ fontSize: "1.8rem" }} />
+        </button>
+        <div className={styles.image}>
+          <img
+            src={props.product.image_url}
+            onError={({ target }) => {
+              target.src = "./assets/images/no_img.jpg";
+            }}
+            alt={props.product.name}
+          />
+        </div>
+        <div className={styles.details}>
+          <p>ชื่อสินค้า</p>
+          <h3>{props.product.name}</h3>
+          <p>รายละเอียด</p>
+          <h3>{props.product.details}</h3>
+          <p>สี</p>
+          <h3>{props.product.color}</h3>
+          <p>ยี่ห้อ</p>
+          <h3>{props.product.brand}</h3>
+          <p>ราคา</p>
+          <h3>{`฿${props.product?.price}`}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductDetails;
